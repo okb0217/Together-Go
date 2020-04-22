bdc8187ad37347a9c2378d7c356e55b4705896e5
okb0217 <scandal217.@gmail.com>
2020-04-22 13:52:26 +0000

    [add]request_show_index

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,16 +1,30 @@
 class RequestsController < ApplicationController
 
   def index
-    @requests1 = Request.where(partner_user_id:current_user.id)
-    @requests2 = @requests1.where.not()
+    requests_for_me = Request.where(partner_user_id: current_user.id)
+    my_requests = Request.where(user_id: current_user.id)
+    filtered = []
+    requests_for_me.each do |req|
+      begin
+        my_requests.find_by!(partner_user_id:req.user_id)
+      rescue
+        filtered.push req
+      end
+    end
+    @requests = filtered
   end
 
   def show
+    @request = Request.find(params[:id])
   end
 
   def new
+    if current_user.parties.count == 0
+      redirect_to user_path(current_user.id), notice: "先にパーティー登録を行って下さい！"
+    else
   	@party = Party.find(params[:party_id])
   	@request = Request.new
+    end
   end
 
   def create
@@ -22,8 +36,8 @@ class RequestsController < ApplicationController
 
   def destroy
     @party = Party.find(params[:party_id])
-  	request = Request.find_by(user_id:current_user.id, partner_party_id:@party.id)
-  	request.destroy
+  	@request = Request.find_by(user_id:current_user.id, partner_party_id:@party.id)
+  	@request.destroy
   	redirect_to party_path(@party.id)
   end
 
